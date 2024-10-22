0860c9a23e87a4477914e8176eed21498dbb24b8
evansmurangiri <94174746+evansmurangiri@users.noreply.github.com>
2024-10-23 02:15:34 +0300

    Added the sessions

--- Tasks/Task/views.py
+++ Tasks/Task/views.py
@@ -3,22 +3,24 @@ from django import forms
 from django.http import HttpResponseRedirect
 from  django.urls import reverse
 
-tasks=["Clean", "wash"]
+
 
 class NewTaskForm(forms.Form):
     task=forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"]=[]
     return render(request, "Task/index.html",{
-        "tasks":tasks
+        "tasks":request.session["tasks"]
     })
 def add(request):
     if request.method == "POST" :
         form =NewTaskForm(request.POST)
         if form.is_valid():
             task=form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("Task:index"))
 
 
